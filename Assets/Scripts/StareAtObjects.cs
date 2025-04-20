0925017f9bb33ac874ceeab65ff3bdafb9eb0369
Mickey Krekelberg <mickkrek.art@gmail.com>
2025-04-21 00:58:36 +1000

    Stare at objects script system this was so tricky

---
+++ Assets/Scripts/StareAtObjects.cs
@@ -0,0 +1,112 @@
+using System;
+using Ghoulish.InteractionSystem;
+using UnityEngine;
+using Unity.Mathematics;
+using UnityEngine.InputSystem.XR;
+
+public class StareAtObjects : MonoBehaviour
+{
+    private Vector3 _currentLookPos;
+    [SerializeField] private Transform _headBone;
+    [Tooltip("This is an offset from the headbone's local position. Position to default to when nothing to stare at.")]
+    [SerializeField] private float _fallbackLookPos;
+    [SerializeField] private float _interactRange = 2f;
+    [SerializeField] private LayerMask _interactLayerMask = default;
+    [SerializeField] private StaringBone[] staringBones;
+    
+    [Serializable]
+    private class StaringBone
+    {
+        public Transform bone;
+        [Tooltip("Maximum local rotation range allowed, from original rotation")]
+        public Vector3 clamp;
+        [Tooltip("How much will this bone rotate to look at the target")]
+        [Range(0f,1f)]
+        public float influence;
+        [Tooltip("How many seconds per frame this bone rotates")]
+        public float speed;
+        [HideInInspector] public Quaternion originalLocalRotation;
+        [HideInInspector] public Quaternion storedQuaternion;
+    }
+
+    void Start()
+    {
+        foreach(StaringBone b in staringBones)
+        {
+            b.originalLocalRotation = b.bone.localRotation;
+        }
+    }
+    void LateUpdate()
+    {
+        Transform nearestTarget = GetNearestTarget();
+        if (nearestTarget == null || !GameManager.Instance._characterMovementEnabled)
+        {
+            _currentLookPos = _headBone.position + (_fallbackLookPos * this.transform.forward);
+        }
+        else
+        {
+            _currentLookPos = nearestTarget.position;
+        }
+        StareAtTarget(_currentLookPos);  
+    }
+    private Transform GetNearestTarget()
+    {
+        Vector3 colliderPosition = transform.position + (transform.forward * 0.5f); //move collider position in front of player for more natural interaction
+        Collider[] colliderArray = Physics.OverlapSphere(colliderPosition, _interactRange, _interactLayerMask);
+        foreach (Collider collider in colliderArray)
+        {
+            if (collider.TryGetComponent(out StareTarget _))
+            {
+                return collider.transform;
+            }
+        }
+        return null;
+    }
+
+    private void StareAtTarget(Vector3 target)
+    {
+        //float distanceToTarget = Vector3.Distance(_currentLookPos, _headBone.position);
+        //float remappedDistance = math.remap(distanceToTarget, 0f, _interactRange, 1f, 0f);
+        foreach(StaringBone b in staringBones)
+        {
+            //store the animator's bone changes
+            Quaternion animatedBoneRot = b.bone.rotation;
+            //restore the stored rotations to counteract the animator's changes
+            b.bone.rotation = b.storedQuaternion;
+            
+            //Get the direction to the target as a worldspace rotation
+            Vector3 targetDirection = (target - b.bone.position).normalized;
+            Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
+            //Weight how much the bone's worldspace rotation should be influenced
+            Quaternion weightedRotation = Quaternion.Slerp(animatedBoneRot, targetRotation, b.influence);
+            //Smoothly rotate toward the desired worldspace rotation
+            weightedRotation = Quaternion.RotateTowards(b.bone.rotation, weightedRotation, b.speed);
+            //Clamp the bone's local euler angle - why was this so hard!
+            Vector3 worldEuler = weightedRotation.eulerAngles;
+            Vector3 originalWorldEuler = (b.bone.parent.rotation * b.originalLocalRotation).eulerAngles;
+            worldEuler.x = ClampAngle(worldEuler.x, originalWorldEuler.x - b.clamp.x, originalWorldEuler.x + b.clamp.x);
+            worldEuler.y = ClampAngle(worldEuler.y, originalWorldEuler.y - b.clamp.y, originalWorldEuler.y + b.clamp.y);
+            worldEuler.z = ClampAngle(worldEuler.z, originalWorldEuler.z - b.clamp.z, originalWorldEuler.z + b.clamp.z);
+            b.bone.eulerAngles = worldEuler;
+
+            //Store the changed rotation because the animator is going to override it next frame
+            b.storedQuaternion = b.bone.rotation;
+        }
+    }
+    public static float ClampAngle(float angle, float min, float max)
+    {
+        float start = (min + max) * 0.5f - 180;
+        float floor = Mathf.FloorToInt((angle - start) / 360) * 360;
+        return Mathf.Clamp(angle, min + floor, max + floor);
+    }
+
+    //This function works but NOT in combination with animated bones...
+    public static Vector3 SmoothDampEuler(Vector3 current, Vector3 target, ref Vector3 currentVelocity, float smoothTime)
+    {
+        return new Vector3(
+            Mathf.SmoothDampAngle(current.x, target.x, ref currentVelocity.x, smoothTime),
+            Mathf.SmoothDampAngle(current.y, target.y, ref currentVelocity.y, smoothTime),
+            Mathf.SmoothDampAngle(current.z, target.z, ref currentVelocity.z, smoothTime)
+        );
+    }
+}
